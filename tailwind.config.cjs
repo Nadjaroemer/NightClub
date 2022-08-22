a9e6f5a323e9744273cdedb766a2394200ce3993
Nadjaroemer <nadja.kuti@gmail.com>
2022-08-22 16:41:07 +0200

    Added hero component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./index.html",
-      "./src/**/*.{vue,js,ts,jsx,tsx}",
-    
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      current: "currentColor",
+      primary: "#030203",
+      secondary: "#ff2a70",
+      grey: "#e0e0e0",
+      white: "ffffff",
+    },
+    extend: {
+      fontFamily: {
+        Ubuntu: ["Ubuntu", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
